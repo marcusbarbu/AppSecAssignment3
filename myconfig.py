a9c543fdfc94c1488842aa474998ded50fd756a7
marcusbarbu <mgb.barbu@gmail.com>
2019-12-01 17:43:54 -0500

    little things for gradescope's sake

--- myconfig.py
+++ myconfig.py
@@ -1,5 +1,7 @@
 import os
 
+os.environ["SECKEY"] = "ThisWouldBeSetOnADeployedSystemButImPuttingItHereForNow"
+
 myconfig = {'SECRET_KEY':os.environ.get("SECKEY"), 
           'SQLALCHEMY_DATABASE_URI':"sqlite:///test.db",
           'SQLALCHEMY_TRACK_MODIFICATIONS':True
